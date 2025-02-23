5663a44381841a1943095c48662e031c4b22f279
Zeenath <zeeezeenu510@gmail.com>
2025-02-23 12:09:14 +0530

    Add files via upload

---
+++ zeeee project.py
@@ -0,0 +1,44 @@
+import cv2
+import os
+import string
+
+img = cv2.imread("C:/Users/admin/Downloads/projjj/mypic.jpg.jpeg") # Replaced with the correct image path
+
+msg = input("Enter secret message:")
+password = input("Enter a passcode:")
+
+d = {}
+c = {}
+
+for i in range(255):
+    d[chr(i)] = i
+    c[i] = chr(i)
+
+m = 0
+n = 0
+z = 0
+
+for i in range(len(msg)):
+    img[n, m, z] = d[msg[i]]
+    n = n + 1
+    m = m + 1
+    z = (z + 1) % 3
+
+cv2.imwrite("encryptedImage.jpg", img)
+os.system("start encryptedImage.jpg")  # Use 'start' to open the image on Windows
+
+message = ""
+n = 0
+m = 0
+z = 0
+
+pas = input("Enter passcode for Decryption")
+if password == pas:
+    for i in range(len(msg)):
+        message = message + c[img[n, m, z]]
+        n = n + 1
+        m = m + 1
+        z = (z + 1) % 3
+    print("Decryption message:", message)
+else:
+    print("YOU ARE NOT auth")
